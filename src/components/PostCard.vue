244b22cad1f0ff2c62915a3c9eabc8c20174bcba
Valht <valeryhdeztamayo@gmail.com>
2024-12-06 12:11:27 -0500

    complement

---
+++ src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="post-card">
+        <h3>Titulo</h3>
+        <p>Parrafo</p>
+        <PostButton></PostButton>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import PostButton from './PostButton';
+</script>
+
+<style scoped>
+    .post-card {
+        border-radius: 15px;
+        padding: 10px;
+        margin: 5px;
+        border: 1px solid #ccc;
+    }
+</style>
